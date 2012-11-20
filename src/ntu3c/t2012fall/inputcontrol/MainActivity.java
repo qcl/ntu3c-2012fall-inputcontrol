127beddfeaedb9a1aa8d29276e2f24be880e5b9f
Qing-Cheng Li <qc.linux@gmail.com>
2012-11-20 21:08:37 +0800

    Create android project and add buttons

---
+++ src/ntu3c/t2012fall/inputcontrol/MainActivity.java
@@ -0,0 +1,22 @@
+package ntu3c.t2012fall.inputcontrol;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
